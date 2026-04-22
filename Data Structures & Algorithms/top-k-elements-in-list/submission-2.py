cfae456d42c52b3dd2f4dcf7108fb7b5c664a3db
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2026-04-22 15:35:27 +0200

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    #     freq_map = dict()
+    #     result = []
+    #     # O(n)
+    #     for n in nums:
+    #         freq_map[n] = freq_map.get(n, 0) + 1
+        
+    #     # O(nlogn)
+    #     value_sort = sorted(freq_map.values(), reverse=True)[:k]
+    #     value_set = set(value_sort)
+        
+    #     # O(n)
+    #     for key, v in freq_map.items():
+    #         if v in value_set: # O(1)
+    #             result.append(key)
+
+    #     return result
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_map = dict()
+        result = []
+        # O(n)
+        for n in nums:
+            freq_map[n] = freq_map.get(n, 0) + 1
+        
+        heap = [] # min-heap
+        for key in freq_map.keys():
+            heapq.heappush(heap, (freq_map[key], key))
+            # after popping the smallest everytime, heap will contain the max k
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+        
+        return result
+        
+
